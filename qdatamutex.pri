19e6123a2bebd193aa6d39ca84f83e32b4e821c5
Vladimir Agadzhanov <vova.skiller@gmail.com>
2021-06-22 15:42:01 +0300

    - Added blocking execution of a lambda expression with a return result. - Added routing between locker object and pointer to stored data - Fixed integration with QOption if it is not required - Edited comments - Added QDataMutex file for inclusion in a Qt-style project

--- qdatamutex.pri
+++ qdatamutex.pri
@@ -2,6 +2,7 @@ HEADERS += \
         $$PWD/src/qdatamutex.h
 
 INCLUDEPATH += $$PWD/src/
+INCLUDEPATH += $$PWD/
 
 CONFIG(QOPTION_INCLUDED) {
         message("QOption already included")
@@ -9,4 +10,7 @@ CONFIG(QOPTION_INCLUDED) {
         INCLUDEPATH += $$PWD/QOption
         DISTFILES += $$PWD/QOption/QOption
 }
+
+DISTFILES += \
+        $$PWD/QDataMutex
 
--- src/qdatamutex.h
+++ src/qdatamutex.h
@@ -1,107 +1,148 @@
-﻿#ifndef QDATAMUTEX_H
+#ifndef QDATAMUTEX_H
 #define QDATAMUTEX_H
 
 #include <mutex>
+#include <functional>
+#ifdef QOPTION_INCLUDED
 #include <QOption>
-///@brief Class who providing storage for multi-thread safe blocking interaction with stored object of @e T type
-template< class T>
+#endif
+
+/*! \brief  Template class who providing storage for multi-thread safe blocking interaction with stored object. */
+template <class T>
 class QDataMutex
 {
-public:
     class locker
     {
     public:
-        /* deleted */
-        locker(const locker & o) = delete;
-        locker&operator = (const locker & o) = delete;
-
-        // empty locker
-        locker(std::mutex * m)
-            : pmtx(m), pvalue(nullptr) { }
-
-        ///@brief create new locker
-        ///@arg pv - data raw pointer
-        ///@arg m - already locked mutex!
+        /*!
+         * \brief   Constructor.
+         * \arg     value - raw pointer to stored value
+         *          mtx - already locked mutex!
+         */
         locker(T * value, std::mutex * mtx)
             : pmtx(mtx), pvalue(value) { }
 
-        ///@brief Move constructor
         locker(locker && b) noexcept {
             pmtx = b.pmtx;
             b.pmtx = nullptr;
             pvalue = b.pvalue;
         }
 
+        /* deleted */
+        locker(const locker & o) = delete;
+        locker& operator=(const locker & o) = delete;
+
         ~locker() {
             if(pmtx)
                 pmtx->unlock();
         }
 
-        ///@brief Returns data raw pointer
-        T * data() const { return pvalue; }
+        /*!
+         * \brief   Returns value ref.
+         * \note    WARNING: reference lifetime must be less than lifetime of the locker object.
+         */
+        T& value() const
+        { return *pvalue; }
 
-        ///@brief Returns const data raw pointer
-        const T * constData() const { return pvalue; }
+        /*!
+         * \brief   Provides access to value pointer.
+         * \note    WARNING: pointer lifetime must be less than lifetime of the locker object.
+         */
+        T* operator->() const noexcept
+        { return pvalue; }
 
-        ///@brief Returns QOptional container with data raw pointer
-        QOption<T*> optional() const {
+        /*! \brief  Returns const data raw pointer. */
+        const T * constData() const noexcept
+        { return pvalue; }
+
+        /*!
+         * \brief   Returns value pointer.
+         * \note    WARNING: pointer lifetime must be less than lifetime of the locker object.
+         */
+        T * data() const noexcept
+        { return pvalue; }
+
+#ifdef QOPTION_INCLUDED
+        /*! \brief  Returns QOptional container with data raw pointer. */
+        QOption<T*> optional() const noexcept {
             if(pvalue)
                 return pvalue;
+
             return None();
         }
+#endif
 
     private:
+        /*! \brief  Pointer to locked mutex. */
         std::mutex * pmtx {nullptr};
+
+        /*! \brief  Pointer to stored value. */
         T * pvalue {nullptr};
     };
 
+public:
     /* deleted */
     QDataMutex(const QDataMutex & o) = delete;
     QDataMutex(QDataMutex && o) = delete;
     QDataMutex & operator=(QDataMutex && o) = delete;
+    QDataMutex & operator=(const QDataMutex & o) = delete;
 
-    ///@brief Copy constructor
     QDataMutex(const T & v)
         : value(v) { }
 
-    ///@brief Move constructor
+    QDataMutex()
+        : value(T()) { }
+
     QDataMutex(T && v) noexcept
         : value(std::forward<T>(v)) { }
 
-    ///@brief Returns @e locker object. This call will blocked if some locker already exists
+    /*!
+     * \brief   Returns locker object.
+     * \note    This call will blocked if some locker already exists.
+     */
     locker lock() {
         mtx.lock();
         return locker(&value, &mtx);
     }
 
-    ///@brief Execute lambda expression with T* argument. This call will blocked if some locker already exists
-    template< typename _Callable>
-    void locked(_Callable && fn) {
+    /*!
+     * \brief   Execute lambda expression in lock mode and returns _Res type value.
+     * \note    Lambda must take as an argument a reference to an object of type
+     *          This call will blocked if some locker already exists.
+     */
+    template<typename _Res = void>
+    _Res locked(std::function<_Res(T&)> && fn) {
         auto b = lock();
-        fn(b.data());
+        return fn(b.value());
+    }
+
+    /*!
+     * \brief   Execute lambda expression in lock mode.
+     * \note    Lambda must take as an argument a reference to an object of type
+     *          This call will blocked if some locker already exists.
+     */
+    template<typename _Callable>
+    void locked(_Callable && fn) {
+        locked<void>(fn);
     }
 
     QDataMutex & operator=(const T& o) {
         auto l = lock();
-        *l.data() = o;
+        l.value() = o;
         return *this;
     }
 
     QDataMutex & operator=(T && o) {
         auto l = lock();
-        *l.data() = std::forward<T>(o);
+        l.value() = std::forward<T>(o);
         return *this;
     }
 
-    QDataMutex & operator=(const QDataMutex & o) {
-        auto l = lock();
-        auto ol = o.lock();
-        *l.data() = *ol.data();
-        return *this;
-    }
-
-private:
+private:    
     std::mutex mtx;
+
+    /*! \brief  Stored value. */
     T value;
 };
 #endif // DATAMUTEX_H
+
